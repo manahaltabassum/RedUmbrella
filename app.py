8c5b64af9c85dd353ece8ba40641e905dec27862
Manahal Tabassum <mtabassum@stuy.edu>
2017-10-08 17:44:36 -0400

    Added files

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask,render_template, request, session, redirect, url_for
+
+app = Flask (__name__)
+
+app.secret_key = 'secret'
+
+userCode = "sasha"
+passCode = "manahal"
+
+
+
+@app.route('/', methods = ['POST', 'GET'])
+
+def root():
+    if session.has_key('user'):
+        #return render_template('welcome.html', name= session['user'])
+        return redirect(url_for('welcome', name = session['user']))
+    else:
+        return render_template('form.html')
+
+
+@app.route('/welcome', methods = ['POST', 'GET'])
+
+def welcome():
+    
+    currentUser = request.form['username']
+    currentPass = request.form['password']
+                               
+    if currentUser  == userCode and currentPass == passCode:
+        session['user'] = currentUser
+        return render_template('welcome.html', name = currentUser)
+
+    
+    elif currentUser != userCode:  
+        return render_template('error.html', errorMsg = "Username is wrong!")
+
+    
+    else:
+        return render_template('error.html' , errorMsg = "Password is wrong!")
+
+@app.route('/logOut' , methods = ['POST', 'GET'])
+
+def logOut():
+    session.pop('user')
+    #return render_template('logOut.html')
+    return redirect(url_for('root'))
+
+if __name__ == '__main__':
+    app.run(debug= True)
+    
